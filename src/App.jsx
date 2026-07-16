c90c513451f2ad575c5c5a7ca6d914c0353608a2
Abdullah Khan <abdullahiftikhar3355@gmail.com>
2026-07-16 22:02:50 +0500

    Create responsive navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Skills from "./pages/Skills"
+import Contact from "./pages/Contact"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div>Portfolio</div>
+    <>
+
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="projects" element={<Projects/>}/>
+      <Route path="/skills" element={<Skills/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
+import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
 
 
 function Navbar() {
-  return (
-    <div>
+    return (
         <nav>
-            Navbar
+            <div className="logo">
+                <h1>Abdullah Khan</h1>
+            </div>
+
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/skills">Skills</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
         </nav>
-    </div>
-  )
+    )
 }
 
 export default Navbar   
